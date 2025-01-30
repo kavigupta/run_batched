f269ea2e3fce1b2f8711b01e5ce265f6cb6ad7c2
Kavi Gupta <kavi@berkeley.edu>
2025-01-29 19:44:29 -0500

    add actual code

---
+++ tests/test_basic.py
@@ -0,0 +1,24 @@
+import unittest
+
+import numpy as np
+
+from run_batched import run_batched
+
+
+def instrumented_double(x, batch_size_callback):
+    batch_size_callback(len(x))
+    return 2 * x
+
+
+class TestBasic(unittest.TestCase):
+    def test_addition(self):
+        batch_sizes = []
+        self.assertEqual(
+            run_batched(
+                lambda x: instrumented_double(x, batch_sizes.append),
+                np.array([1, 2, 3]),
+                2,
+            ).tolist(),
+            [2, 4, 6],
+        )
+        self.assertEqual(batch_sizes, [2, 1])
